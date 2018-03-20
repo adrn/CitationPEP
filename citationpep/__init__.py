2671f2d2cf03e078c8f1234dedc35ec3118e25f9
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-03-20 08:09:13 -0400

    Link back to init

--- citationpep/__init__.py
+++ citationpep/__init__.py
@@ -1,3 +1,4 @@
 from .core import *
 
-__citation__ = 'This is a demo citation!'
+__citation__ = 'This is a demo citation! See ' \
+    + 'https://github.com/adrn/CitationPEP/blob/master/citationpep/__init__.py'
